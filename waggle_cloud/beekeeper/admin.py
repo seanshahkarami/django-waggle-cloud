31145b06c66ce4349fa82c90633844e5f36eef5b
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-03-21 23:09:24 -0500

    added additional display and search fields to node

--- waggle_cloud/beekeeper/admin.py
+++ waggle_cloud/beekeeper/admin.py
@@ -3,5 +3,5 @@ from . import models
 
 
 admin.site.register(models.Beehive, list_display=("name", "description", "created"))
-admin.site.register(models.Node, list_display=("vsn", "node_id", "beehive", "created"), list_filter=("beehive__name",))
+admin.site.register(models.Node, list_display=("vsn", "node_id", "beehive", "created", "registered", "owner"), list_filter=("beehive__name",), search_fields=("vsn", "node_id", "owner__username", "beehive__name"))
 admin.site.register(models.NodeRegistrationToken, list_display=("key", "user", "beehive", "created", "expires"), search_fields=("user__username", "beehive__name"))
