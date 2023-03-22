3b6f3b52afb0b4daecb51a211f1ec6d3cb781fa8
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-03-21 21:22:01 -0500

    updated url

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("waggle_cloud.ecr.urls")),
+    path("api/", include("waggle_cloud.ecr.urls")),
 ]
